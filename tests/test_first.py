1721eb66d84aed6f8e4bd1eba721d49eb4aee2e5
Igor <igor.doudelzak@gmail.com>
2024-04-12 15:01:45 +0200

    separating alembic test

--- tests/test_first.py
+++ tests/test_first.py
@@ -13,12 +13,3 @@ DATABASE_URI = os.getenv('DATABASE_URI')
 
 def test_check():
     assert 2 == 2
-
-
-def check_alembic_integrity():
-    engine = sqlalchemy.create_engine(DATABASE_URI)
-    alembic_cfg = config.Config('alembic.ini')
-    script_ = script.ScriptDirectory.from_config(alembic_cfg)
-    with engine.begin() as conn:
-        context = migration.MigrationContext.configure(conn)
-        assert context.get_current_revision() == script_.get_current_head()
