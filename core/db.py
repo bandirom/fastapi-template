9c485c32952e5393f46f429578829e77cac49210
Nazarii <romanchenko@procivis.ch>
2025-04-12 13:56:13 +0300

    Update project

--- core/db.py
+++ core/db.py
@@ -3,4 +3,4 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from . import settings
 
 engine = create_async_engine(url=settings.database_uri.unicode_string(), echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+async_session = async_sessionmaker(bind=engine, expire_on_commit=False, autocommit=False, autoflush=False)
